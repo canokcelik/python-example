45f5d9e448b9a249d6dbe80970f9fe44e8ea8e91
canokcelik <canokcelik@hotmail.com>
2021-02-17 13:31:50 +0300

    Update app.py

--- python-zero-downtime/app.py
+++ python-zero-downtime/app.py
@@ -6,7 +6,7 @@ hostname = os.environ.get('HOSTNAME','localhost')
 
 @app.route('/')
 def hello_world():
-    return hostname+'v4 - 15sn gecikmeli kalkis - Hey gidi koca dunya \n'
+    return hostname+'v4 - 15sn gecikmeli kalkis \n'
 
 if __name__ == '__main__':
     time.sleep(15) # baslarken 15 saniye gecik, sonradan ayaga kalk
