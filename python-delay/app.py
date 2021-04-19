1b36c17f9b3ee96e4a499b4648b88494030ba941
Can Okcelik <Can.Okcelik@tav.aero>
2021-04-19 14:03:03 +0300

    blue/green test editing

--- python-delay/app.py
+++ python-delay/app.py
@@ -6,8 +6,8 @@ hostname = os.environ.get('HOSTNAME','localhost')
 
 @app.route('/')
 def hello_world():
-    time.sleep(300)
-    return hostname+'v4 - 5dk gecikmeli kalkis \n'
+    time.sleep(5)
+    return hostname+'v4 - blue deployment \n'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
